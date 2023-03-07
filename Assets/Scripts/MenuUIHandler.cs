584322a7d317cdd30f812f460e49959f048beb8e
A-J-Hanson <101951368+A-J-Hanson@users.noreply.github.com>
2023-03-07 11:46:36 +0000

    menu UI exit button functional

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 // Set the script to be executed later. Best practice for UIs
 [DefaultExecutionOrder(1000)]
@@ -19,4 +22,14 @@ public void StartNew()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
 }
+
